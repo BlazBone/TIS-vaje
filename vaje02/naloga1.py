eae3704f39f08cd7e7a5a04991adddcbb8b0d7f0
BlazBone <bb6846@student.uni-lj.si>
2022-03-01 20:10:22 +0100

    dela za eno crko za par trojka manjka

--- vaje02/naloga1.py
+++ vaje02/naloga1.py
@@ -1,6 +1,49 @@
+from itertools import count
 from math import log2
-from nis import match
+# from nis import match
 from typing import Counter
+
+
+def crkeFun(besedilo):
+    H = 0
+    a = Counter(str(besedilo))
+    vse_crke = sorted(a)
+    b = {}
+    for crka in vse_crke:
+        b[crka] = a[crka]/len(besedilo)
+        H -= b[crka]*log2(b[crka])
+    return H
+
+
+def pariFun(besedilo):
+    H1 = crkeFun(besedilo=besedilo)
+    H2 = 0
+
+    posamezno = list(besedilo)
+    pari = list(zip(posamezno[:-1], posamezno[1:]))
+    verjetnost_dvojckov = {}
+    for a, b in set(pari):
+        verjetnost_dvojckov[a+b] = besedilo.count(a+b)/len(pari)
+        H2 -= verjetnost_dvojckov[a+b] * log2(verjetnost_dvojckov[a+b])
+    return H2 - H1
+
+
+def trojkeFun(besedilo):
+    H1 = crkeFun(besedilo)
+    H2 = pariFun(besedilo=besedilo)
+    posamezno = list(besedilo)
+    H3 = 0
+    verjetnost_trojk = {}
+    trojke = list(zip(posamezno[:-2], posamezno[1:-1], posamezno[2:]))
+    print(trojke[:11])
+    print(len(trojke))
+    print(len(set(trojke)))
+
+    for a, b, c in set(trojke):
+        verjetnost_trojk[a+b+c] = besedilo.count(a+b+c)/len(trojke)
+        H3 -= verjetnost_trojk[a+b+c]*log2(verjetnost_trojk[a+b+c])
+
+    return H3-H2-H1
 
 
 def naloga1(besedilo, p):
@@ -20,35 +63,19 @@ def naloga1(besedilo, p):
             racunamo povprecno informacijo na znak abecede
             pri enem poznanem predhodnemu znaku
         p = 2: H(X3|X1,X2)
-
     Returns
     -------
     H : float
         Povprecna informacija na znak abecede z upostevanjem
         stevila poznanih predhodnih znakov 'p'
     """
-    besedilo = besedilo.strip().replace(".", '').replace(',', '').replace(
-        ";", '').lower().replace("'", "").replace(":", '').replace("?", '').replace(" ", '').replace("-", '').replace('(', '').replace(')', '').replace("!", '')
-    text_len = len(besedilo)
-
-    a = Counter(str(besedilo))
-    vse_crke = sorted(a)
-    print(vse_crke)
-    if p == 1:
-        b  = {}
-        print("1")
-        H = 0
-        for crka in vse_crke:
-            b[crka] = a[crka]/text_len
-            H -= b[crka]*log2(b[crka])
+    besedilo = "".join(filter(lambda a: a.isalpha(), besedilo)).lower()
+    H = 0
+    if p == 0:
+        H = crkeFun(besedilo=besedilo)
+    elif p == 1:
+        H = pariFun(besedilo=besedilo)
     elif p == 2:
-        b1 = {}
-        for crka in vse_crke:
-            b1[crka] = a[crka]/text_len
-        print('2')
-        
-    elif p == 3:
-        print("3")
+        H = trojkeFun(besedilo=besedilo)
 
-    print(H)
     return H
--- vaje02/test_naloga1.py
+++ vaje02/test_naloga1.py
@@ -27,6 +27,7 @@ def test_naloga1(case_dir, case_id):
 
 
 if __name__ == '__main__':
+    # test_naloga1('./primeri', 4)
     test_naloga1('./primeri', 3)
-    test_naloga1('./primeri', 2)
-    test_naloga1('./primeri', 1)
+    # test_naloga1('./primeri', 2)
+    # test_naloga1('./primeri', 1)
