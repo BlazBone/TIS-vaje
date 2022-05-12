65f902c5a51d1e1a7839499d24a6b8ffae0f390d
BlazBone <bb6846@student.uni-lj.si>
2022-05-12 13:37:32 +0200

    added old tests and fixed

--- naloga4/naloga4.py
+++ naloga4/naloga4.py
@@ -1,6 +1,6 @@
 import numpy as np
-from scipy.io.wavfile import write
-from matplotlib import pyplot as plt
+# from scipy.io.wavfile import write
+# from matplotlib import pyplot as plt
 # def naloga4(vhod: list, fs: int) -> str:
 def naloga4(vhod,fs):
     """
@@ -34,25 +34,30 @@ def naloga4(vhod,fs):
 
     tones = {'C1':261.63, 'CIS1':277.18, 'D1':293.66, 'DIS1':311.13,'E1':329.63,'F1':349.23, 'FIS1':369.99, 'G1':392, 'GIS1':415.30,'A1':440,'B1':466.16,'H1':493.88,
              'C2':523.25,'CIS2':554.37,'D2':587.33,'DIS2':622.25,'E2':659.25,'F2':698.46,'FIS2':739.99,'G2':783.99,'GIS2':830.61,'A2':880,'B2':932.33,'H2':987.77 }
-    accords = {'Cdur':"C1E1G1",'Cmol':'C1DIS1G1','Ddur':'D1FIS1A1','Dmol':'D1F1A1','Edur':'E1GIS1H1','Fdur':'F1A1C2','Fmol':'F1GIS1C2'
+    accords = {'Cdur':"C1E1G1",'Cmol':'C1DIS1G1','Ddur':'D1FIS1A1','Dmol':'D1F1A1','Edur':'E1GIS1H1','Emol':'E1G1H1','Fdur':'F1A1C2','Fmol':'F1GIS1C2'
                ,'Gdur':'G1H1D2','Gmol':'G1B1D2','Amol':'A1C2E2','Adur':'A1CIS2E2','Hmol':'H1D2FIS2','Hdur':'H1DIS2FIS2'}
     
-    figure, axis = plt.subplots()
+    # figure, axis = plt.subplots()
     # axis[0].plot(t,x, color='blue')
     # axis[0].set_title("jakost v odvisnosti od casa")
     # axis[0].set_xlabel("t[s]")
     # print(P)
-    axis.plot(f,P, color='red')
-    axis.set_title("moc v  odvisnosti od frekvence")
-    axis.set_xlabel("f[Hz]")
+    # axis.plot(f,P, color='red')
+    # axis.set_title("moc v  odvisnosti od frekvence")
+    # axis.set_xlabel("f[Hz]")
     # plt.show()
     # f/P
+    # print(P)
+    nparr = np.array(P)
+    novp = nparr/np.max(nparr)
+    # print(novp)
+    print(np.max(nparr))
     freqTonov = []
-    for index,size in enumerate(P):
+    for index,size in enumerate(novp):
         if index > len(P)/2:
             break
             
-        if size > 200:
+        if size > 0.01:
             # print(index,size)
             freq = index * (fs/N)
 
@@ -60,21 +65,17 @@ def naloga4(vhod,fs):
     
     end_tones = []
     for frq in freqTonov:
-        found = False
         for key,value in tones.items():
             if abs(frq-value) < 1:
-                found = True
                 end_tones.append(key)
         
-        # if not found:
-        #     return "";
     print(end_tones)
     end_tones = list(dict.fromkeys(end_tones))
-                
     print(end_tones)
+    # print([4,5,6,7]/2)
     together = ""
-    if len(end_tones) <= 3:
-        for tone in end_tones:
+    if len(end_tones) <= 5:
+        for tone in end_tones[0:3:1]:
             together += tone
     elif len(end_tones) == 6:
         togetherNizji = ""
--- naloga4/test_naloga4.py
+++ naloga4/test_naloga4.py
@@ -75,7 +75,10 @@ def main(case_dir="primeri", case_id=None, *other):
 
 if __name__ == "__main__":
     # Argumenti ukazne vrstice so v sys.argv
-    main(*sys.argv[1:])
+    # main(*sys.argv[1:])
+    
+    for i in range(10):
+        test_naloga4('old_json', i+1)
     # test_naloga4('primeri', 1)
     # test_naloga4('primeri', 2)
     # test_naloga4('primeri', 3)
